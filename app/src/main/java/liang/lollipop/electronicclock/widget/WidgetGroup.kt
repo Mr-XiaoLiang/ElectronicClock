3d92cedf3398ffb71592a051d16549101f4aedd4
lollipop <liujian594@hotmail.com>
2019-07-31 10:37:17 +0800

    更新小部件容器算法

--- app/src/main/java/liang/lollipop/electronicclock/widget/WidgetGroup.kt
+++ app/src/main/java/liang/lollipop/electronicclock/widget/WidgetGroup.kt
@@ -1,6 +1,7 @@
 package liang.lollipop.electronicclock.widget
 
 import android.content.Context
+import android.graphics.Point
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.util.Size
@@ -35,7 +36,17 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
 
     private val tmpRect1 = Rect()
     private val tmpRect2 = Rect()
+    private val tmpPoint = Point()
 
+    private var spanCountX = 0
+    private var spanCountY = 0
+
+    /**
+     * 格子数量
+     * 这个数量是指窄边的格子数
+     * 长边的数量会依据此数量来进行变化
+     * 尽量保证格子更加接近正方形
+     */
     var gridCount = 6
         set(value) {
             field = value
@@ -43,12 +54,27 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
             requestLayout()
         }
 
+    /**
+     * 添加面板
+     * 添加View的唯一途径
+     * Group需要根据panel的一些参数决定小部件怎么进行布局排版
+     */
     fun addPanel(panel: Panel) {
         if (panel.view != null) {
             panel.create(layoutInflater, this)
         }
         panelList.add(panel)
         super.addView(panel.view)
+    }
+
+    /**
+     * 移除一个面板
+     */
+    fun removePanel(panel: Panel) {
+        // 如果在集合中成功移除了面板，那么也从View中移除相应的View
+        if (panelList.remove(panel)) {
+            super.removeView(panel.view)
+        }
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
@@ -65,9 +91,59 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
         }
         val offsetX = paddingLeft
         val offsetY = paddingTop
-
+        for (i in 0 until childCount) {
+            val view = getChildAt(i)
+            val panel = findPanelByView(view)
+            if (view.visibility == View.GONE) {
+                panel.layout(0, 0, 0, 0)
+                continue
+            }
+        }
     }
 
+    /**
+     * 找到一个符合要求的格子，并且返回它的坐标
+     * 返回的坐标为格子的左上角格子坐标
+     */
+    private fun findGrid(spanX: Int, spanY: Int, panel: Panel? = null): Point {
+        // 遍历每一个格子，直到找到一个符合要求的位置
+        tmpRect1.set(0, 0, spanX * gridSize.width, spanY * gridSize.height)
+        var meet = false
+        for (x in 0 until spanCountX) {
+            for (y in 0 until spanCountY) {
+                // 格子尺寸不变，只做位置的偏移
+                tmpRect1.offsetTo(x * gridSize.width, y * gridSize.height)
+                tmpRect1.offset(paddingLeft, paddingTop)
+                // 每次检查前，默认为当前格子符合要求
+                meet = true
+                for (p in panelList) {
+                    // 如果是自身，那么跳过检查
+                    if (p == panel) {
+                        continue
+                    }
+                    p.copyBounds(tmpRect2)
+                    // 如果和某一个面板存在交集，那么认为这个位置不可用
+                    // 结束检查，开始下一轮的循环
+                    if (hasIntersection(tmpRect1, tmpRect2)) {
+                        meet = false
+                        break
+                    }
+                }
+                // 如果遇见了可以使用的格子，那么提前返回
+                if (meet) {
+                    tmpPoint.set(x, y)
+                    return tmpPoint
+                }
+            }
+        }
+        tmpPoint.set(-1, -1)
+        return tmpPoint
+    }
+
+    /**
+     * 计算格子尺寸
+     * 将按照指定的格子数对当前容器进行栅格处理
+     */
     private fun calculateGridSize(w: Int, h: Int) {
         val xSize: Int
         val ySize: Int
@@ -75,14 +151,23 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
         // 尽量得到更接近方形的单元格
         if (w < h) {
             xSize = w / gridCount
+            spanCountX = gridCount
             ySize = calculateScale(h, xSize)
+            spanCountY = h / ySize
         } else {
             ySize = h / gridCount
+            spanCountY = gridCount
             xSize = calculateScale(w, ySize)
+            spanCountX = w / xSize
         }
         gridSize = Size(xSize, ySize)
     }
 
+    /**
+     * 对某一线段进行依据参照值的分段
+     * 确保每段的长度更接近参照值
+     * @return 返回每段的长度值
+     */
     private fun calculateScale(length: Int, src: Int): Int {
         // 如果长度能够被参照值整除，那么直接返回参照值
         if (length % src == 0) {
@@ -120,6 +205,10 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
         }
     }
 
+    /**
+     * 根据View返回panel的对象
+     * 如果找不到对应的panel，那么将会抛出异常
+     */
     private fun findPanelByView(view: View): Panel {
         for (panel in panelList) {
             if (panel.view == view) {
@@ -129,6 +218,11 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
         throw InflateException("Found a view that does not correspond to a panel")
     }
 
+    /**
+     * 是否可以放置
+     * 检查面板是否可以被放置
+     * @return 如果为true，表示可以被放置
+     */
     private fun canPlace(panel: Panel): Boolean {
         for (p in panelList) {
             if (panel == p) {
@@ -141,16 +235,41 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
         return true
     }
 
+    /**
+     * 两个面板之间是否存在交集
+     * 如果存在交集，那么返回true
+     */
     private fun hasIntersection(p0: Panel, p1: Panel): Boolean {
+        // 复制得到面板对应的View尺寸及位置
         p0.copyBounds(tmpRect1)
-        tmpRect1.selfCheck()
         p1.copyBounds(tmpRect2)
-        tmpRect2.selfCheck()
-        val vertical = tmpRect1.bottom <= tmpRect2.top || tmpRect1.top >= tmpRect2.bottom
-        val horizontal = tmpRect1.right <= tmpRect2.left || tmpRect1.left >= tmpRect2.right
+        return hasIntersection(tmpRect1, tmpRect2)
+    }
+
+    /**
+     * 检查两个矩形之间是否存在交集
+     */
+    private fun hasIntersection(r1: Rect, r2: Rect): Boolean {
+        // 如果其中一个是空的，那么认为两者之间没有交集
+        if (r1.isEmpty || r2.isEmpty) {
+            return false
+        }
+        r1.selfCheck()
+        r2.selfCheck()
+        // 判断纵向是否存在交集，如果不存在，那么表示为通过
+        val vertical = r1.bottom <= r2.top || r1.top >= r2.bottom
+        // 判断横向是否存在交集，如果不存在，那么表示为通过
+        val horizontal = r1.right <= r2.left || r1.left >= r2.right
+        // 如果横向或者纵向不存在交集，那么认为两个矩形处于平行状态
+        // 否则认为有交集
         return !(vertical || horizontal)
     }
 
+    /**
+     * 矩形的自检方法
+     * 当右边小于左边时，交换左右位置，
+     * 使面积及形状不变，但是尺寸正确
+     */
     private fun Rect.selfCheck() {
         if (this.left > this.right) {
             val tmp = this.right
@@ -164,6 +283,11 @@ class WidgetGroup(context: Context, attr: AttributeSet?, defStyleAttr: Int, defS
         }
     }
 
+    /**
+     * 当panel不能被摆放时，会将不能摆放的panel放置在此处，
+     * 然后通过回调函数传出，外部可以选择调整面板尺寸
+     * 或者移除此面板
+     */
     private fun cantLayoutPanel(panel: Panel) {
         // TODO
     }
