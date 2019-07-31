bc79a20b4c1485c0fbc94fe7d7d94840755c69e7
lollipop <liujian594@hotmail.com>
2019-07-31 22:02:58 +0800

    更新小部件容器算法

---
+++ app/src/main/java/liang/lollipop/electronicclock/panel/ClockPanel.kt
@@ -0,0 +1,18 @@
+package liang.lollipop.electronicclock.panel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import liang.lollipop.electronicclock.widget.Panel
+import liang.lollipop.electronicclock.widget.PanelInfo
+
+/**
+ * @author lollipop
+ * @date 2019-07-31 15:19
+ * 时钟的面板
+ */
+class ClockPanel(info: PanelInfo): Panel(info) {
+    override fun onCreateView(layoutInflater: LayoutInflater, parent: ViewGroup): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
