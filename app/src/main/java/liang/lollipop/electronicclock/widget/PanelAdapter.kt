65084832061c9321976b6d402c6da495e4a71f9c
Lollipop <liujian594@hotmail.com>
2019-08-03 22:57:21 +0800

    更新了小部件容器的算法

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/PanelAdapter.kt
@@ -0,0 +1,14 @@
+package liang.lollipop.electronicclock.widget
+
+/**
+ * @author lollipop
+ * @date 2019-08-03 19:23
+ * 面板的适配器
+ */
+object PanelAdapter {
+
+    fun <I: PanelInfo> createPanelByInfo(info: I): Panel<I> {
+        TODO("尚未实现")
+    }
+
+}
