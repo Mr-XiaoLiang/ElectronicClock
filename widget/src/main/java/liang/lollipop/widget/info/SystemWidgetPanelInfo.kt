5c5801ce019a444221ddd82aa5fc75fc375e905f
lollipop <liujian594@hotmail.com>
2019-08-05 13:59:09 +0800

    添加系统小部件面板

---
+++ widget/src/main/java/liang/lollipop/widget/info/SystemWidgetPanelInfo.kt
@@ -0,0 +1,11 @@
+package liang.lollipop.widget.info
+
+import liang.lollipop.widget.widget.PanelInfo
+
+/**
+ * @author lollipop
+ * @date 2019-08-05 13:25
+ * 系统小部件的面板信息
+ */
+class SystemWidgetPanelInfo: PanelInfo() {
+}
