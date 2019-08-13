8572a9197b95a759a8c8e58226d1206875907c1a
Lollipop <liujian594@hotmail.com>
2019-08-13 22:26:41 +0800

    添加偏好设置的页面

---
+++ app/src/main/java/liang/lollipop/electronicclock/activity/PreferencesActivity.kt
@@ -0,0 +1,12 @@
+package liang.lollipop.electronicclock.activity
+
+import android.os.Bundle
+import liang.lollipop.electronicclock.R
+
+class PreferencesActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+    }
+}
