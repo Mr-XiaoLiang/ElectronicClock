adfe8e650062945500c6225c9713bfd6e245406b
Lollipop <liujian594@hotmail.com>
2019-08-15 22:46:21 +0800

    添加偏好设置的列表组件

---
+++ app/src/main/java/liang/lollipop/electronicclock/list/PreferenceHolder.kt
@@ -0,0 +1,20 @@
+package liang.lollipop.electronicclock.list
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author lollipop
+ * @date 2019-08-15 22:13
+ * Boolean类型的偏好设置holder
+ */
+abstract class PreferenceHolder<V, T: PreferenceInfo<V>>(view: View,
+         protected val onSelectedListener: OnSelectedListener<V>): RecyclerView.ViewHolder(view) {
+
+    abstract fun onBind(info: T)
+
+    interface OnSelectedListener<V> {
+        fun onSelected(holder: PreferenceHolder<*, *>, newValue: V)
+    }
+
+}
---
+++ app/src/main/java/liang/lollipop/electronicclock/list/PreferenceNumber.kt
@@ -0,0 +1,15 @@
+package liang.lollipop.electronicclock.list
+
+/**
+ * @author lollipop
+ * @date 2019-08-15 22:13
+ * Boolean类型的偏好设置holder
+ */
+class PreferenceNumber : PreferenceInfo<Int>() {
+
+    override var value: Int = 0
+
+    var max = Int.MAX_VALUE
+    var min = 0
+
+}
