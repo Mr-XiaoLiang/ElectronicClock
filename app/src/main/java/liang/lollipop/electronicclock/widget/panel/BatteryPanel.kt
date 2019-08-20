1b205267cd6622f9c03577f2053a02f7ef005232
lollipop <liujian594@hotmail.com>
2019-08-20 21:21:20 +0800

    增加电池面板的绘制类

--- app/src/main/java/liang/lollipop/electronicclock/widget/panel/BatteryPanel.kt
+++ app/src/main/java/liang/lollipop/electronicclock/widget/panel/BatteryPanel.kt
@@ -1,5 +1,9 @@
 package liang.lollipop.electronicclock.widget.panel
 
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,4 +19,40 @@ class BatteryPanel(info: BatteryInfo): Panel<BatteryInfo>(info) {
     override fun onCreateView(layoutInflater: LayoutInflater, parent: ViewGroup): View {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+
+
+    private class BatteryDrawable(private var info: BatteryInfo): Drawable() {
+
+        fun onInfoChange(newInfo: BatteryInfo) {
+            info = newInfo
+            relayout()
+        }
+
+        private fun relayout() {
+
+        }
+
+        override fun onBoundsChange(bounds: Rect?) {
+            super.onBoundsChange(bounds)
+            relayout()
+        }
+
+        override fun draw(canvas: Canvas) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun setAlpha(alpha: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getOpacity(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun setColorFilter(colorFilter: ColorFilter?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
 }
