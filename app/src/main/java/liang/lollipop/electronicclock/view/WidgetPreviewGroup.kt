3deefdbdc2bd29c778c2accd52693d8d121cb074
Lollipop <liujian594@hotmail.com>
2019-08-23 16:34:46 +0800

    完成小部件预览容器的开发

--- app/src/main/java/liang/lollipop/electronicclock/view/WidgetPreviewGroup.kt
+++ app/src/main/java/liang/lollipop/electronicclock/view/WidgetPreviewGroup.kt
@@ -2,7 +2,9 @@ package liang.lollipop.electronicclock.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
+import kotlin.math.min
 
 /**
  * 为小部件预览的容器
@@ -32,6 +34,39 @@ class WidgetPreviewGroup(context: Context, attr: AttributeSet?, defStyleAttr: In
             requestLayout()
         }
 
+    private fun getChildSize(width: Int, height: Int): IntArray {
+        val gridSize = min(width / spanX, height / spanY)
+        return intArrayOf(spanX * gridSize, spanY * gridSize)
+    }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom
+        val childSize = getChildSize(widthSize, heightSize)
+        for (i in 0 until childCount) {
+            val view = getChildAt(i)
+            if (view.visibility != View.VISIBLE) {
+                continue
+            }
+            measureChild(view, childSize[0], childSize[1])
+        }
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        val widthSize = right - left - paddingLeft - paddingRight
+        val heightSize = bottom - top - paddingTop - paddingBottom
+        val childSize = getChildSize(widthSize, heightSize)
+        val childLeft = (widthSize - childSize[0]) / 2 + paddingLeft
+        val childTop = (heightSize - childSize[1]) / 2 + paddingTop
+        for (i in 0 until childCount) {
+            val view = getChildAt(i)
+            if (view.visibility != View.VISIBLE) {
+                continue
+            }
+            view.layout(childLeft, childTop, childLeft + childSize[0], childTop + childSize[1])
+        }
+    }
 
 }
