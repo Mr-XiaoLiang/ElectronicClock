a1a6f95ca70f220c7e5665bdffd0375d66808bc6
Lollipop <liujian594@hotmail.com>
2019-08-25 15:53:32 +0800

    更新通用小部件调整页面

---
+++ app/src/main/java/liang/lollipop/electronicclock/fragment/PanelInfoAdjustmentFragment.kt
@@ -0,0 +1,69 @@
+package liang.lollipop.electronicclock.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+/**
+ * @author lollipop
+ * @date 2019-08-25 15:23
+ * 面板信息调整的Fragment
+ */
+abstract class PanelInfoAdjustmentFragment: Fragment() {
+
+    companion object {
+        const val ARG_INFO_ID = "ARG_INFO_ID"
+    }
+
+    private var infoId = ""
+
+    private var infoLoadCallback: InfoLoadCallback? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            infoId = it.getString(ARG_INFO_ID)?:""
+        }
+    }
+
+    /**
+     * 向外部提供Panel的View
+     */
+    abstract fun getPanelView(): View
+
+    /**
+     * 开始加载
+     */
+    protected fun startLoading() {
+        infoLoadCallback?.onInfoLoadStatusChange(true)
+    }
+
+    /**
+     * 停止加载
+     */
+    protected fun stopLoading() {
+        infoLoadCallback?.onInfoLoadStatusChange(false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (infoId.isNotEmpty()) {
+
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is InfoLoadCallback) {
+            infoLoadCallback = context
+        } else {
+            throw RuntimeException("context is not interface InfoLoadCallback")
+        }
+    }
+
+    interface InfoLoadCallback {
+        fun onInfoLoadStatusChange(isLoading: Boolean)
+    }
+
+}
