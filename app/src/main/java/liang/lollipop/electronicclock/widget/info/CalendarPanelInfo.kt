fd92a94264a223a6f83f44449cf850ad0618dc73
lollipop <liujian594@hotmail.com>
2019-09-23 23:11:22 +0800

    开始日历小部件的制作

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/info/CalendarPanelInfo.kt
@@ -0,0 +1,11 @@
+package liang.lollipop.electronicclock.widget.info
+
+import liang.lollipop.widget.widget.PanelInfo
+
+/**
+ * @author lollipop
+ * @date 2019-09-23 23:03
+ * 日历面板的描述信息
+ */
+class CalendarPanelInfo: PanelInfo() {
+}
