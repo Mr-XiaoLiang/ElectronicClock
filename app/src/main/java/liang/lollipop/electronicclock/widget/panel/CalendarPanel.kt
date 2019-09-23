c8135d9d842e97d09799acdede8f50c128ebfa4d
Lollipop <liujian594@hotmail.com>
2019-09-23 23:11:22 +0800

    开始日历小部件的制作

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/panel/CalendarPanel.kt
@@ -0,0 +1,18 @@
+package liang.lollipop.electronicclock.widget.panel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import liang.lollipop.electronicclock.widget.info.CalendarPanelInfo
+import liang.lollipop.widget.widget.Panel
+
+/**
+ * @author lollipop
+ * @date 2019-09-23 23:09
+ * 日历面板的对象
+ */
+class CalendarPanel(info: CalendarPanelInfo): Panel<CalendarPanelInfo>(info) {
+    override fun onCreateView(layoutInflater: LayoutInflater, parent: ViewGroup): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
