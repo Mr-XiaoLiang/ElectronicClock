17eb477d3e94bbb24a4b4692596c8e9726c2104c
lollipop <liujian594@hotmail.com>
2019-09-25 23:07:58 +0800

    开始日历小部件的制作

--- app/src/main/java/liang/lollipop/electronicclock/widget/info/CalendarPanelInfo.kt
+++ app/src/main/java/liang/lollipop/electronicclock/widget/info/CalendarPanelInfo.kt
@@ -1,5 +1,6 @@
 package liang.lollipop.electronicclock.widget.info
 
+import android.graphics.Color
 import liang.lollipop.widget.widget.PanelInfo
 
 /**
@@ -8,4 +9,20 @@ import liang.lollipop.widget.widget.PanelInfo
  * 日历面板的描述信息
  */
 class CalendarPanelInfo: PanelInfo() {
+
+    /**
+     * 只展示月份模式
+     */
+    var onlyMonth = false
+
+    /**
+     * 今天的日期前景色
+     */
+    var todayColor = Color.WHITE
+
+    /**
+     * 今天的日期背景色
+     */
+    var todayBgColor = Color.BLUE
+
 }
