0ac4b29d7f4fe91090abc1fafe6fc0344deaf212
lollipop <liujian594@hotmail.com>
2019-10-03 03:03:44 +0800

    推进日历组件的开发进度

--- app/src/main/java/liang/lollipop/electronicclock/view/CalendarView.kt
+++ app/src/main/java/liang/lollipop/electronicclock/view/CalendarView.kt
@@ -1,9 +1,15 @@
 package liang.lollipop.electronicclock.view
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
+import liang.lollipop.electronicclock.utils.LunarCalendar
+import kotlin.math.min
 
 /**
  * @author lollipop
@@ -19,7 +25,14 @@ class CalendarView(context: Context, attrs: AttributeSet?, defStyleAttr:Int )
     private var onSizeChangeListener: ((view: CalendarView,
                                         w: Int, h: Int, oldw: Int, oldh: Int) -> Unit)? = null
 
-//    private var
+    /**
+     * 参数设置
+     */
+    var options: Options = Options()
+        set(value) {
+            field = value
+            notifyDataChange()
+        }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -30,11 +43,67 @@ class CalendarView(context: Context, attrs: AttributeSet?, defStyleAttr:Int )
         onSizeChangeListener?.invoke(this, w, h, oldw, oldh)
     }
 
+    fun notifyDataChange() {
+        requestLayout()
+        // 其他设置
+    }
+
+    private class DayView(context: Context) : View(context) {
+
+        var element: LunarCalendar.Element = LunarCalendar.Element()
+
+        val options = Options()
+
+        private val bounds = RectF()
+
+        private val paint = Paint().apply {
+            isDither = true
+            isAntiAlias = true
+            style = Paint.Style.FILL_AND_STROKE
+        }
+
+        fun notifyDataChange() {
+            requestLayout()
+            // 其他设置
+        }
+
+        override fun onDraw(canvas: Canvas?) {
+            super.onDraw(canvas)
+            canvas?:return
+            val radius = min(bounds.width(), bounds.height()) / 2
+            // 背景绘制
+            paint.color = if (element.isToday) { options.todayBgColor } else { options.otherBgColor }
+            if (options.isOvalBg) {
+                canvas.drawCircle(bounds.centerX(), bounds.centerY(), radius, paint)
+            } else {
+                canvas.drawRect(bounds, paint)
+            }
+
+        }
+
+        override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+            super.onLayout(changed, left, top, right, bottom)
+            bounds.set(paddingLeft.toFloat(),
+                paddingTop.toFloat(),
+                (right - left) - paddingRight.toFloat(),
+                (bottom - top) - paddingBottom.toFloat())
+        }
+
+        override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+            super.onSizeChanged(w, h, oldw, oldh)
+            bounds.set(paddingLeft.toFloat(),
+                paddingTop.toFloat(),
+                w - paddingRight.toFloat(),
+                h - paddingBottom.toFloat())
+        }
+
+    }
+
     enum class Type {
         Month, Week, Day
     }
 
-    class Options {
+    open class Options {
         /** 星期 **/
         var isShowWeek = true
         /** 农历 **/
@@ -59,6 +128,10 @@ class CalendarView(context: Context, attrs: AttributeSet?, defStyleAttr:Int )
         var isStartingOnSunday = true
         /** 是否显示日程 **/
         var isShowSchedule = true
+        /** 节日小点的颜色 **/
+        var festivalPointColor = Color.BLUE
+        /** 吉日的小点颜色 **/
+        var auspiciousPointColor = Color.RED
 
         /** 主要文字尺寸比例 **/
         var mainFontSizeWeight = 0.24F
@@ -66,6 +139,27 @@ class CalendarView(context: Context, attrs: AttributeSet?, defStyleAttr:Int )
         var secondaryFontSizeWeight = 0.16F
         /** 标识点的尺寸比例 **/
         var identificationSizeWeight = 0.06F
+
+        fun copy(other: Options) {
+            isShowWeek               = other.isShowWeek
+            isShowLunar              = other.isShowLunar
+            isShowFestival           = other.isShowFestival
+            isShowSolarTerms         = other.isShowSolarTerms
+            isShowAuspicious         = other.isShowAuspicious
+            isOvalBg                 = other.isOvalBg
+            todayTextColor           = other.todayTextColor
+            todayBgColor             = other.todayBgColor
+            otherTextColor           = other.otherTextColor
+            otherBgColor             = other.otherBgColor
+            isStartingOnSunday       = other.isStartingOnSunday
+            isShowSchedule           = other.isShowSchedule
+            festivalPointColor       = other.festivalPointColor
+            auspiciousPointColor     = other.auspiciousPointColor
+            mainFontSizeWeight       = other.mainFontSizeWeight
+            secondaryFontSizeWeight  = other.secondaryFontSizeWeight
+            identificationSizeWeight = other.identificationSizeWeight
+        }
+
     }
 
 }
