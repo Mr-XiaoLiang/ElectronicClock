2185d16272c1429b34fb187e5eb71a06c4546334
lollipop <liujian594@hotmail.com>
2019-10-08 20:35:41 +0800

    推进日历组件的开发进度

---
+++ app/src/main/java/liang/lollipop/electronicclock/bean/AdjustmentSelect.kt
@@ -0,0 +1,18 @@
+package liang.lollipop.electronicclock.bean
+
+/**
+ * @author lollipop
+ * @date 2019-10-08 20:33
+ */
+class AdjustmentSelect(run: AdjustmentSelect.() -> Unit):
+    AdjustmentInfo() {
+
+    val itemList = ArrayList<String>()
+
+    var selectedIndex = -1
+
+    fun addItem(vararg items: String) {
+        itemList.addAll(items)
+    }
+
+}
