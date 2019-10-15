f4de5e7b57c1f6ebd8be41d8cb53804c4db6a957
Lollipop <liujian594@hotmail.com>
2019-10-16 00:13:11 +0800

    增加屏幕保护服务

--- app/src/main/java/liang/lollipop/electronicclock/service/ClockDreamService.kt
+++ app/src/main/java/liang/lollipop/electronicclock/service/ClockDreamService.kt
@@ -1,11 +1,31 @@
 package liang.lollipop.electronicclock.service
 
 import android.service.dreams.DreamService
+import liang.lollipop.electronicclock.R
 
 /**
  * @author lollipop
  * @date 2019-10-15 23:54
  * 时钟的屏保服务
+ *
  */
 class ClockDreamService: DreamService() {
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        setContentView(R.layout.activity_widget)
+    }
+
+    override fun onDreamingStarted() {
+        super.onDreamingStarted()
+
+    }
+
+    override fun onDreamingStopped() {
+        super.onDreamingStopped()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+    }
 }
