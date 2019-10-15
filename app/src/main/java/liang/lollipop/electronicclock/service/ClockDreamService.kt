7b823c99d7cb40191f1ba7aac46e6c6be70beea8
lollipop <liujian594@hotmail.com>
2019-10-15 23:55:20 +0800

    增加屏幕保护服务

---
+++ app/src/main/java/liang/lollipop/electronicclock/service/ClockDreamService.kt
@@ -0,0 +1,11 @@
+package liang.lollipop.electronicclock.service
+
+import android.service.dreams.DreamService
+
+/**
+ * @author lollipop
+ * @date 2019-10-15 23:54
+ * 时钟的屏保服务
+ */
+class ClockDreamService: DreamService() {
+}
