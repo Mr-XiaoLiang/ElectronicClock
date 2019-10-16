fb8985fed6e4395d914cf86490c7816ae012db80
Lollipop <liujian594@hotmail.com>
2019-10-17 00:26:07 +0800

    增加屏幕保护服务

--- app/src/main/java/liang/lollipop/electronicclock/service/ClockDreamService.kt
+++ app/src/main/java/liang/lollipop/electronicclock/service/ClockDreamService.kt
@@ -1,7 +1,14 @@
 package liang.lollipop.electronicclock.service
 
 import android.service.dreams.DreamService
+import android.view.View
+import android.view.WindowInsets
 import liang.lollipop.electronicclock.R
+import liang.lollipop.electronicclock.utils.PreferenceHelper
+import liang.lollipop.electronicclock.view.ContentLoadingProgressBar
+import liang.lollipop.widget.WidgetHelper
+import liang.lollipop.widget.info.ClockPanelInfo
+import liang.lollipop.widget.widget.WidgetGroup
 
 /**
  * @author lollipop
@@ -11,21 +18,86 @@ import liang.lollipop.electronicclock.R
  */
 class ClockDreamService: DreamService() {
 
+    private var widgetHelper: WidgetHelper? = null
+
+    private val widgetGroup: WidgetGroup?
+        get() {
+            return findViewById(R.id.widgetGroup)
+        }
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        setContentView(R.layout.activity_widget)
+        // Exit dream upon user touch
+        isInteractive = false
+        // Hide system UI
+        isFullscreen = true
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        setContentView(R.layout.dream_widget)
+        initView()
+        initData()
+    }
+
+    private fun initView() {
+        widgetGroup?.let { group ->
+            insetListener(group) { insets ->
+                val left = insets.systemWindowInsetLeft
+                val top = insets.systemWindowInsetTop
+                val right = insets.systemWindowInsetRight
+                val bottom = insets.systemWindowInsetBottom
+                group.setPadding(left, top, right, bottom)
+            }
+            widgetHelper = PreferenceHelper.createWidgetHelper(this, group)
+        }
+
+    }
+
+    private fun initData() {
+        val loadingView = findViewById<ContentLoadingProgressBar>(R.id.loadingView)
+        loadingView.putColorForRes(R.color.colorAccent, R.color.colorPrimary)
+        widgetHelper?.updateByDB { status ->
+            if (status == WidgetHelper.LoadStatus.START) {
+                loadingView.show()
+            } else {
+                loadingView.hide()
+                if (widgetHelper?.panelCount?:0 < 1) {
+                    onWidgetEmpty()
+                }
+            }
+        }
+    }
+
+    private fun onWidgetEmpty() {
+        widgetGroup?.post {
+            widgetGroup?.let {
+                val clockPanelInfo = ClockPanelInfo()
+                clockPanelInfo.sizeChange(it.spanCountX, it.spanCountY)
+                clockPanelInfo.offset(0, 0)
+                widgetHelper?.addPanel(clockPanelInfo)
+            }
+        }
     }
 
     override fun onDreamingStarted() {
         super.onDreamingStarted()
-
+        widgetHelper?.onStart()
     }
 
     override fun onDreamingStopped() {
         super.onDreamingStopped()
+        widgetHelper?.onStop()
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
+    }
+
+    private fun insetListener(group: View, onInsetChange: (insets: WindowInsets) -> Unit) {
+        group.fitsSystemWindows = true
+        group.setOnApplyWindowInsetsListener { _, insets ->
+            onInsetChange(insets)
+            insets.consumeStableInsets()
+        }
     }
 }
