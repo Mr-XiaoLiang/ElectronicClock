26dbc4c9ebe123d411909ffa0752162647bf5aac
lollipop <liujian594@hotmail.com>
2019-10-28 22:11:40 +0800

    开始相框小部件的制作

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/info/PhotoFramePanelInfo.kt
@@ -0,0 +1,14 @@
+package liang.lollipop.electronicclock.widget.info
+
+import liang.lollipop.widget.widget.PanelInfo
+
+/**
+ * @author lollipop
+ * @date 2019-10-28 22:00
+ * 相框的面板描述信息
+ */
+class PhotoFramePanelInfo: PanelInfo() {
+
+
+
+}
---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/panel/PhotoFramePanel.kt
@@ -0,0 +1,20 @@
+package liang.lollipop.electronicclock.widget.panel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import liang.lollipop.electronicclock.widget.info.PhotoFramePanelInfo
+import liang.lollipop.widget.widget.Panel
+
+/**
+ * @author lollipop
+ * @date 2019-10-28 22:01
+ * 相框的面板
+ */
+class PhotoFramePanel(info: PhotoFramePanelInfo): Panel<PhotoFramePanelInfo>(info) {
+    override fun onCreateView(layoutInflater: LayoutInflater, parent: ViewGroup): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
