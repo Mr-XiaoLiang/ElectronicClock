b1e46e4636f20f9dc76b26ddeb36054e6a7e0b10
lollipop <liujian594@hotmail.com>
2019-10-29 20:38:54 +0800

    开始相框小部件的制作

---
+++ app/src/main/java/liang/lollipop/electronicclock/utils/OutlineHelper.kt
@@ -0,0 +1,15 @@
+package liang.lollipop.electronicclock.utils
+
+import android.graphics.Bitmap
+import android.view.View
+
+/**
+ * @author lollipop
+ * @date 2019-10-29 20:36
+ * 边缘轮廓线的辅助类
+ */
+class OutlineHelper(targetView: View) {
+
+    private var outLineBitmap: Bitmap? = null
+
+}
