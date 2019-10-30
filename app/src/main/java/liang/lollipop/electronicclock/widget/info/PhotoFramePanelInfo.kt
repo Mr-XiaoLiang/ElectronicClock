9ac11b2ae4cbe970da24db7b0c482fcc4afae418
lollipop <liujian594@hotmail.com>
2019-10-30 20:05:47 +0800

    开始相框小部件的制作

--- app/src/main/java/liang/lollipop/electronicclock/widget/info/PhotoFramePanelInfo.kt
+++ app/src/main/java/liang/lollipop/electronicclock/widget/info/PhotoFramePanelInfo.kt
@@ -1,6 +1,8 @@
 package liang.lollipop.electronicclock.widget.info
 
 import liang.lollipop.widget.widget.PanelInfo
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * @author lollipop
@@ -9,6 +11,31 @@ import liang.lollipop.widget.widget.PanelInfo
  */
 class PhotoFramePanelInfo: PanelInfo() {
 
+    companion object {
+        const val KEY_IMAGES = "KEY_IMAGES"
+    }
 
+    val images = ArrayList<String>()
+
+    override fun parse(jsonObj: JSONObject) {
+        super.parse(jsonObj)
+        val imageArray = jsonObj.optJSONArray(KEY_IMAGES)
+        images.clear()
+        if (imageArray != null) {
+            for (i in 0 until imageArray.length()) {
+                val img = imageArray.optString(i)?:continue
+                images.add(img)
+            }
+        }
+    }
+
+    override fun serialize(jsonObj: JSONObject) {
+        super.serialize(jsonObj)
+        val imageArray = JSONArray()
+        for (img in images) {
+            imageArray.put(img)
+        }
+        jsonObj.put(KEY_IMAGES, imageArray)
+    }
 
 }
