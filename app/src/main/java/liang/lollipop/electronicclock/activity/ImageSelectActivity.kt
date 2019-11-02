86785f72952e6554fcc547049ba4ea9eb9f2c630
lollipop <liujian594@hotmail.com>
2019-11-02 23:07:34 +0800

    开始相框小部件的制作

---
+++ app/src/main/java/liang/lollipop/electronicclock/activity/ImageSelectActivity.kt
@@ -0,0 +1,13 @@
+package liang.lollipop.electronicclock.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import liang.lollipop.electronicclock.R
+
+class ImageSelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select)
+    }
+}
