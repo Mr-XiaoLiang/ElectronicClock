9e043d9282e88df2dc9c074ca9d0af5938052ca6
Lollipop <liujian594@hotmail.com>
2019-11-22 21:52:34 +0800

    完成功能增加，版本推进

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/info/LauncherPanelInfo.kt
@@ -0,0 +1,11 @@
+package liang.lollipop.electronicclock.widget.info
+
+import liang.lollipop.widget.widget.PanelInfo
+
+/**
+ * @author lollipop
+ * @date 2019-11-22 21:51
+ * 启动器面板的Info
+ */
+class LauncherPanelInfo: PanelInfo() {
+}
