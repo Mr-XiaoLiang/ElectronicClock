beb41d1e16b51d5a8fdbfa3ed60caeed52edcf49
lollipop <liujian594@hotmail.com>
2019-11-22 21:52:34 +0800

    完成功能增加，版本推进

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/panel/LauncherPanel.kt
@@ -0,0 +1,12 @@
+package liang.lollipop.electronicclock.widget.panel
+
+import liang.lollipop.electronicclock.widget.info.LauncherPanelInfo
+import liang.lollipop.widget.widget.Panel
+
+/**
+ * @author lollipop
+ * @date 2019-11-22 21:50
+ * 启动器面板
+ */
+class LauncherPanel(info: LauncherPanelInfo): Panel<LauncherPanelInfo>(info) {
+}
