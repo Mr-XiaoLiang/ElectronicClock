f3a5bcf6ff79aeeb2c3189195eab2efdeae7aa08
Lollipop <liujian594@hotmail.com>
2019-11-28 22:47:24 +0800

    调整方向，制作单独的绘制结构，不再包装View

--- app/src/main/java/liang/lollipop/electronicclock/offScreen/OffScreenEngine.kt
+++ app/src/main/java/liang/lollipop/electronicclock/offScreen/OffScreenEngine.kt
@@ -21,87 +21,42 @@ class OffScreenEngine(private val context: Context) {
         var FPS = 60
     }
 
-    private val rootView = ViewRoot(context)
-
     private var surface: Surface? = null
 
     private var viewSize = ViewSize(0, 0)
 
-    init {
-        rootView.onRequestLayout {
-            requestLayout()
-        }
-    }
-
-    fun addView(view: View, layoutParams: FrameLayout.LayoutParams) {
-        rootView.addView(view, layoutParams)
-    }
-
-    fun addView(view: View) {
-        rootView.addView(view)
-    }
-
-    fun addView(view: View, width: Int, height: Int) {
-        rootView.addView(view, width, height)
-    }
-
-    fun addView(layoutId: Int) {
-        rootView.addView(LayoutInflater.from(context).inflate(layoutId, rootView, true))
-    }
+    private var padding = Inset(0, 0, 0, 0)
 
     fun setSize(width: Int, height: Int) {
         viewSize.reset(width, height)
-        requestLayout()
-    }
-
-    fun onInsetChange(left: Int, top: Int, right: Int, bottom: Int) {
-        rootView.setPadding(left, top, right, bottom)
     }
 
     fun draw(canvas: Canvas) {
-        rootView.draw(canvas)
     }
 
     fun onShow() {
-        rootView.callAttachedToWindow()
     }
 
     fun onHide() {
-        rootView.callDetachedFromWindow()
     }
 
-    private fun requestLayout() {
-        rootView.measure(View.MeasureSpec.makeMeasureSpec(viewSize.width, View.MeasureSpec.EXACTLY),
-            View.MeasureSpec.makeMeasureSpec(viewSize.height, View.MeasureSpec.EXACTLY))
-        rootView.layout(0, 0, viewSize.width, viewSize.height)
+    interface Painter {
+        fun onSizeChange(left: Int, top: Int, right: Int, bottom: Int)
     }
 
-    private class ViewRoot(context: Context): FrameLayout(context) {
-
-        private var requestCallback: (() -> Unit)? = null
-
-        override fun requestLayout() {
-            super.requestLayout()
-            requestCallback?.invoke()
-        }
-
-        fun callAttachedToWindow() {
-            onAttachedToWindow()
-        }
-
-        fun callDetachedFromWindow() {
-            onDetachedFromWindow()
-        }
-
-        fun onRequestLayout(callback: () -> Unit) {
-            this.requestCallback = callback
-        }
-    }
-
-    private data class ViewSize(var width: Int, var height: Int) {
+    data class ViewSize(var width: Int, var height: Int) {
         fun reset(width: Int, height: Int) {
             this.width = width
             this.height = height
+        }
+    }
+
+    data class Inset(var left: Int, var top: Int,var right: Int, var bottom: Int) {
+        fun reset(left: Int, top: Int, right: Int, bottom: Int) {
+            this.left = left
+            this.top = top
+            this.right = right
+            this.bottom = bottom
         }
     }
 
