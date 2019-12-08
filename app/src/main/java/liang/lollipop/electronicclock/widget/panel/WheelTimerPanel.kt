d4f19588f56c0035fc3a004a5e0bd5fed2ad83cf
lollipop <liujian594@hotmail.com>
2019-12-08 17:57:04 +0800

    推进转盘时钟的开发进度

---
+++ app/src/main/java/liang/lollipop/electronicclock/widget/panel/WheelTimerPanel.kt
@@ -0,0 +1,11 @@
+package liang.lollipop.electronicclock.widget.panel
+
+import liang.lollipop.electronicclock.widget.info.WheelTimerPanelInfo
+import liang.lollipop.widget.widget.Panel
+
+/**
+ * @author lollipop
+ * @date 2019-12-08 17:56
+ */
+class WheelTimerPanel(panelInfo: WheelTimerPanelInfo): Panel<WheelTimerPanelInfo>(panelInfo) {
+}
