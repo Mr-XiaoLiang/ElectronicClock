8b3a8e5ffe3353cdfaedd4adf0357fd59722b995
Lollipop <liujian594@hotmail.com>
2020-01-02 22:48:22 +0800

    添加列表多项选择的对话框

--- app/src/main/java/liang/lollipop/electronicclock/utils/CheckListDialog.kt
+++ app/src/main/java/liang/lollipop/electronicclock/utils/CheckListDialog.kt
@@ -1,9 +1,14 @@
 package liang.lollipop.electronicclock.utils
 
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
 /**
  * @author lollipop
  * @date 2020-01-01 15:30
  * 可以选择的列表对话框
  */
-class CheckListDialog {
+class CheckListDialog: BottomSheetDialogFragment() {
+
+
+
 }
