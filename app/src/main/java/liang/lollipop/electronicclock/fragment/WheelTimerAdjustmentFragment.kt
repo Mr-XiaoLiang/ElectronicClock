394ef354abfe31d5ecbc0344593d8b6527234e73
lollipop <liujian594@hotmail.com>
2020-01-06 21:30:11 +0800

    更新滚轮时间的调试面板

--- app/src/main/java/liang/lollipop/electronicclock/fragment/WheelTimerAdjustmentFragment.kt
+++ app/src/main/java/liang/lollipop/electronicclock/fragment/WheelTimerAdjustmentFragment.kt
@@ -2,6 +2,8 @@ package liang.lollipop.electronicclock.fragment
 
 import android.view.View
 import liang.lollipop.electronicclock.bean.AdjustmentInfo
+import liang.lollipop.electronicclock.widget.info.WheelTimerPanelInfo
+import liang.lollipop.electronicclock.widget.panel.WheelTimerPanel
 import liang.lollipop.widget.widget.PanelInfo
 
 /**
@@ -10,6 +12,10 @@ import liang.lollipop.widget.widget.PanelInfo
  * 滚轮时间的调整页面
  */
 class WheelTimerAdjustmentFragment: PanelInfoAdjustmentFragment() {
+
+    private val wheelTimerPanelInfo = WheelTimerPanelInfo()
+    private val wheelTimerPanel = WheelTimerPanel(wheelTimerPanelInfo)
+
     override fun onInfoChange(info: AdjustmentInfo, newValue: Any) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
