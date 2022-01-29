88179f88afaa766d370b8e837ad8f28fcccfb969
Lollipop <liujian594@hotmail.com>
2022-01-30 00:07:39 +0800

    重构WidgetGroup结构，增加绝对位置的排版方式

---
+++ widget/src/main/java/liang/lollipop/widget/widget/AbsolutelyWidgetGroup.kt
@@ -0,0 +1,138 @@
+package liang.lollipop.widget.widget
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import liang.lollipop.widget.utils.Utils
+import java.util.*
+
+/**
+ * 绝对位置小部件管理器
+ */
+class AbsolutelyWidgetGroup(
+    context: Context,
+    attr: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : WidgetGroup(context, attr, defStyleAttr, defStyleRes) {
+
+    companion object {
+        private val logger = Utils.loggerI("AbsolutelyWeightGroup")
+    }
+
+    constructor(context: Context, attr: AttributeSet?, defStyleAttr: Int) : this(
+        context,
+        attr,
+        defStyleAttr,
+        0
+    )
+
+    constructor(context: Context, attr: AttributeSet?) : this(context, attr, 0)
+    constructor(context: Context) : this(context, null)
+
+    private val priorityList = ArrayList<Panel<*>>()
+
+    /**
+     * 用于实例化Panel中View的上下文
+     */
+    private val layoutInflater = LayoutInflater.from(context)
+
+    /**
+     * 回收复用的Rect对象集合
+     */
+    private val recyclerRectList = LinkedList<Rect>()
+
+    /**
+     * 子View的长按点击事件
+     */
+    private var childLongClickListener: ((panel: Panel<*>) -> Boolean)? = null
+
+    /**
+     * 添加面板
+     * 添加View的唯一途径
+     * Group需要根据panel的一些参数决定小部件怎么进行布局排版
+     */
+    fun addPanel(panel: Panel<*>): Boolean {
+        logger("addPanel()")
+        if (panel.view == null) {
+            panel.create(layoutInflater, this)
+        }
+        panelList.add(panel)
+        addView(panel.view)
+        setChildLongClick(panel)
+        setChildClick(panel)
+        if (isActive) {
+            panel.updatePanelLifecycle(true)
+        }
+        listener?.onPanelAdded(panel)
+        return true
+    }
+
+    /**
+     * 移除一个面板
+     */
+    fun removePanel(panel: Panel<*>) {
+        // 如果被移除的panel是选中的panel，那么就放弃
+        if (panel == selectedPanel) {
+            cancelSelected()
+        }
+        // 如果在集合中成功移除了面板，那么也从View中移除相应的View
+        if (panelList.remove(panel)) {
+            super.removeView(panel.view)
+        }
+    }
+
+    /**
+     * 当子View被长按时，触发
+     */
+    fun onChildLongClick(listener: ((Panel<*>) -> Boolean)?) {
+        childLongClickListener = listener
+        for (p in panelList) {
+            setChildLongClick(p)
+        }
+    }
+
+    private fun cancelSelected() {
+        // TODO
+//        GridWidgetGroup.logger("cancelSelected")
+//        pushPanelWhenTouch()
+//        cancelDragListener?.invoke(selectedPanel)
+//        selectedPanel = null
+//        activeActionId = GridWidgetGroup.NO_ID
+//        invalidate()
+    }
+
+    private fun setChildLongClick(panel: Panel<*>) {
+        if (panel.customClick) {
+            return
+        }
+        val listener = childLongClickListener
+        if (listener != null) {
+            panel.view?.setOnLongClickListener { listener(panel) }
+        } else {
+            panel.view?.setOnLongClickListener(null)
+        }
+    }
+
+    private fun setChildClick(panel: Panel<*>) {
+        if (panel.customClick) {
+            return
+        }
+        panel.view?.setOnClickListener {
+            if (listener?.onChildClick(panel) != true) {
+                panel.callOnClick(it)
+            }
+        }
+    }
+
+    override fun getChildDrawingOrder(childCount: Int, drawingPosition: Int): Int {
+        return super.getChildDrawingOrder(childCount, drawingPosition)
+    }
+
+    fun notifyPanelPriorityChanged() {
+        // TODO
+    }
+
+
+}
